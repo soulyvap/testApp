8b0f0d76e38247bc59db9cd0875ce263a4c4db18
soulyvap <soulyvap@metropolia.fi>
2022-01-12 12:04:34 +0200

    http-b done

--- components/List.js
+++ components/List.js
@@ -1,35 +1,45 @@
-/* eslint-disable prettier/prettier */
-
 import React from 'react';
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
 import {useState, useEffect} from 'react';
+import {baseUrl} from '../utils/variables';
 
 const List = () => {
-  const url =
-    'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
   const [mediaArray, setMediaArray] = useState([]);
-  const loadMedia = async () => {
+  const loadMedia = async (start = 0, limit = 10) => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(
+        `${baseUrl}media?start=${start}&limit=${limit}`
+      );
       if (!response.ok) {
         throw Error(response.statusText);
-      };
+      }
       const json = await response.json();
-      setMediaArray(json);
+      const media = await Promise.all(
+        json.map(async (item) => {
+          const responsePromise = await fetch(
+            baseUrl + 'media/' + item.file_id
+          );
+          const mediaData = await responsePromise.json();
+          console.log(mediaData);
+          return mediaData;
+        })
+      );
+      console.log(media);
+      setMediaArray(media);
     } catch (error) {
       console.error(error);
     }
-    console.log(mediaArray);
+    console.log();
   };
   useEffect(async () => {
-    loadMedia();
+    loadMedia(0, 5);
   }, []);
 
   return (
     <FlatList
       data={mediaArray}
-      keyExtractor={(item) => item.title}
+      keyExtractor={(item) => item.file_id.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Text, View, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
+import {uploadsUrl} from '../utils/variables';
 
 const ListItem = ({singleMedia}) => {
   return (
     <TouchableOpacity style={styles.listItem}>
-      <Image style={styles.image} source={{uri: singleMedia.thumbnails.w160}} />
+      <Image
+        style={styles.image}
+        source={{uri: uploadsUrl + singleMedia.thumbnails?.w160}}
+      />
       <View style={styles.textSection}>
         <Text style={styles.title}>{singleMedia.title}</Text>
         <Text style={styles.content}>{singleMedia.description}</Text>
@@ -21,10 +25,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#ccc',
     marginBottom: 5,
     padding: 20,
+    height: 200,
   },
   image: {
-    width: '45%',
-    height: 'auto',
+    flex: 1,
     marginRight: 10,
     borderRadius: 10,
   },

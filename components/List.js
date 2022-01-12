67f6e525b7e68c67a0e3f3abcb2c7e7b2ef173cb
soulyvap <soulyvap@metropolia.fi>
2022-01-12 12:28:18 +0200

    http-c done

--- components/List.js
+++ components/List.js
@@ -1,41 +1,10 @@
 import React from 'react';
 import {FlatList} from 'react-native';
+import {useMedia} from '../hooks/ApiHooks';
 import ListItem from './ListItem';
-import {useState, useEffect} from 'react';
-import {baseUrl} from '../utils/variables';
 
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-  const loadMedia = async (start = 0, limit = 10) => {
-    try {
-      const response = await fetch(
-        `${baseUrl}media?start=${start}&limit=${limit}`
-      );
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      const json = await response.json();
-      const media = await Promise.all(
-        json.map(async (item) => {
-          const responsePromise = await fetch(
-            baseUrl + 'media/' + item.file_id
-          );
-          const mediaData = await responsePromise.json();
-          console.log(mediaData);
-          return mediaData;
-        })
-      );
-      console.log(media);
-      setMediaArray(media);
-    } catch (error) {
-      console.error(error);
-    }
-    console.log();
-  };
-  useEffect(async () => {
-    loadMedia(0, 5);
-  }, []);
-
+  const {mediaArray} = useMedia();
   return (
     <FlatList
       data={mediaArray}

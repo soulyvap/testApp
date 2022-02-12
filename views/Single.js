26ea4f30fb714c671f4a7b3cf258e3aef3d5f945
soulyvap <soulyvap@metropolia.fi>
2022-02-12 21:55:29 +0200

    ui tweaks

--- views/Single.js
+++ views/Single.js
@@ -2,7 +2,13 @@ import React, {useContext, useEffect, useState} from 'react';
 import {Dimensions, SafeAreaView, StyleSheet, View} from 'react-native';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
-import {Avatar, Button, Card, Text, ThemeProvider} from 'react-native-elements';
+import {
+  Avatar,
+  ButtonGroup,
+  Card,
+  Text,
+  ThemeProvider,
+} from 'react-native-elements';
 import GlobalStyles from '../utils/GlobalStyles';
 import {Video} from 'expo-av';
 import {useFavourite, useTag, useUser} from '../hooks/ApiHooks';
@@ -87,10 +93,16 @@ const Single = ({route}) => {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView style={GlobalStyles.AndroidSafeArea}>
-        <Card containerStyle={{height: '95%'}}>
+        <Card
+          containerStyle={{
+            height: '80%',
+            display: 'flex',
+          }}
+          wrapperStyle={{height: '100%'}}
+        >
           {file.media_type === 'image' ? (
             <Card.Image
-              style={{height: screenHeight * 0.5}}
+              style={styles.image}
               source={{uri: uploadsUrl + file.thumbnails.w640}}
             />
           ) : (
@@ -107,44 +119,44 @@ const Single = ({route}) => {
 
           <View
             style={{
-              marginTop: screenHeight * 0.05,
-              height: screenHeight * 0.2,
               display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              flex: 3,
+              marginTop: 20,
             }}
           >
-            <Avatar source={{uri: avatar}} />
+            <Card.Title style={{textAlign: 'left', fontSize: 20}}>
+              {file.title}
+            </Card.Title>
+            <Text>{file.description}</Text>
             <View
               style={{
                 display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                gap: 5,
-                marginLeft: 10,
+                flexDirection: 'row',
+                alignItems: 'center',
               }}
             >
-              <Card.Title style={{textAlign: 'left', fontSize: 20}}>
-                {file.title}
-              </Card.Title>
-              <Text>{file.description}</Text>
-              <Text>By {username}</Text>
-              <Text>{likes.length + ' likes'}</Text>
-              <Button
-                title="like"
-                disabled={userLike}
-                onPress={() => {
-                  createLike();
-                }}
-              />
-              <Button
-                title="unlike"
-                disabled={!userLike}
-                onPress={() => {
-                  deleteLike();
-                }}
-              />
+              <Avatar source={{uri: avatar}} rounded />
+              <Text style={{marginStart: 10}}>{username}</Text>
             </View>
+            <Text>{likes.length + ' likes'}</Text>
+            <ButtonGroup
+              buttons={['like', 'unlike']}
+              selectedIndex={userLike ? 1 : 0}
+              onPress={(value) => {
+                switch (value) {
+                  case 0:
+                    if (!userLike) createLike();
+                    break;
+                  case 1:
+                    if (userLike) deleteLike();
+                    break;
+                  default:
+                    break;
+                }
+              }}
+            />
           </View>
         </Card>
       </SafeAreaView>
